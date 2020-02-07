7828b79725b47839f69c8f96de773883818ec59b
Marcin Liweń <31144163+marcinliwen@users.noreply.github.com>
2020-02-07 23:40:47 +0100

    colors

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -40,9 +40,9 @@ export const IndexPageTemplate = ({
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
             boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
+              'none',
+            backgroundColor: 'transparent',
+            color: '#333',
             lineHeight: '1',
             padding: '0.25em',
           }}
@@ -55,7 +55,7 @@ export const IndexPageTemplate = ({
             boxShadow:
               'none',
             backgroundColor: 'transparent',
-            color: 'white',
+            color: '#333',
             lineHeight: '1',
             padding: '0.25em',
           }}
