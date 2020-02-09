d59e6bfc706e27bee7034089960cfe4996133c07
marcinliwen <marcin.liwen@sohoshop.pl>
2020-02-09 20:45:48 +0100

    new logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/Untilted.png'
+import logo from '../img/Untitled.png'
 
 const Navbar = class extends React.Component {
   constructor(props) {
