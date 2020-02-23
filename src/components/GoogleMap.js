7fc6af53af2193a0af0ba14c882ff5764a787fb5
marcinliwen <liwen.marcin@gmail.com>
2020-02-23 22:46:33 +0100

    google map

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import GoogleMapReact from 'google-map-react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api'
 
 const defaultProps = {
   center: {
@@ -9,23 +9,22 @@ const defaultProps = {
   zoom: 11,
 }
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>
 
-const GoogleMap = () => (
-  <div style={{ height: '100vh', width: '100%' }}>
-    <GoogleMapReact
-      // bootstrapURLKeys={{ key: 'AlzaSyBRKoTpCZsUXfcXyxHxoK-PpXMHYwfqs8o' }}
-      bootstrapURLKeys={{ key: 'AIzaSyDI_zdFaLx4vnV2Ypd05BqGnBV_nM_a1Jk' }}
-      defaultCenter={defaultProps.center}
-      defaultZoom={defaultProps.zoom}
-    >
-      <AnyReactComponent
-        lat={59.955413}
-        lng={30.337844}
-        text={'Kreyser Avrora'}
-      />
-    </GoogleMapReact>
-  </div>
-)
-
-export default GoogleMap
+class GoogleMapLoad extends Component {
+    render() {
+       return (
+        <LoadScript
+          id="script-loader"
+          googleMapsApiKey="AIzaSyDI_zdFaLx4vnV2Ypd05BqGnBV_nM_a1J"
+        >
+          <GoogleMap
+            id='example-map'
+            defaultCenter={defaultProps.center}
+            defaultZoom={defaultProps.zoom}
+          >
+          </GoogleMap>
+        </LoadScript>
+       )
+    }
+  }
+export default GoogleMapLoad
