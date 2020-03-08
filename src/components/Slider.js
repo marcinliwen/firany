33ef2969e84dca8d37a3a0c50ca8de864b63331b
marcinliwen <liwen.marcin@gmail.com>
2020-03-08 23:41:52 +0100

    slider style

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,16 +2,27 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./slide.sass";
 
 const prev ="<";
 function SampleNextArrow(props) {
     const { className, style, onClick } = props;
     return (
       <div
+        role="button"
+        tabIndex={0}
         className={className}
         style={{ ...style, display: "block", color: "#2f1b0f" }}
         onClick={onClick}
-      />
+        onKeyDown={onClick}
+      >
+        <svg viewBox="0 0 512 512">
+          <path d="M367.954,213.588L160.67,5.872c-7.804-7.819-20.467-7.831-28.284-0.029c-7.819,7.802-7.832,20.465-0.03,28.284
+            l207.299,207.731c7.798,7.798,7.798,20.486-0.015,28.299L132.356,477.873c-7.802,7.819-7.789,20.482,0.03,28.284
+            c3.903,3.896,9.016,5.843,14.127,5.843c5.125,0,10.25-1.958,14.157-5.873l207.269-207.701
+            C391.333,275.032,391.333,236.967,367.954,213.588z"/>
+        </svg>
+      </div>
     );
   }
   
@@ -19,9 +30,20 @@ function SampleNextArrow(props) {
     const { className, style, onClick } = props;
     return (
       <div
+        role="button"
+        tabIndex={0}
         className={className}
         style={{ ...style, display: "block", color: "#2f1b0f" }}
-        onClick={onClick}>{prev}</div>
+        onClick={onClick}
+        onKeyDown={onClick}
+        >{prev}
+        <svg viewBox="0 0 512 512">
+          <path d="M367.954,213.588L160.67,5.872c-7.804-7.819-20.467-7.831-28.284-0.029c-7.819,7.802-7.832,20.465-0.03,28.284
+            l207.299,207.731c7.798,7.798,7.798,20.486-0.015,28.299L132.356,477.873c-7.802,7.819-7.789,20.482,0.03,28.284
+            c3.903,3.896,9.016,5.843,14.127,5.843c5.125,0,10.25-1.958,14.157-5.873l207.269-207.701
+            C391.333,275.032,391.333,236.967,367.954,213.588z"/>
+        </svg>
+        </div>
     
     );
   }
@@ -39,8 +61,8 @@ export default class SimpleSlider extends Component {
     };
     
     return (
-      <div>
-        <h2> Single Item</h2>
+      <div className="prod-container">
+       <h3 className="has-text-weight-semibold is-size-3" >Przykładowe produkty</h3>
         <Slider {...settings}>
           <div>
             <img src={require("../../public/img/f1.jpg")} alt="f1"/>
@@ -61,6 +83,7 @@ export default class SimpleSlider extends Component {
           <img src={require("../../public/img/f6.jpg")} alt="f1"/>
           </div>
         </Slider>
+      
       </div>
     );
   }
